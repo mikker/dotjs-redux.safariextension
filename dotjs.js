5d490bb9239d98f05254d8812ace189861008f80
Mikkel Malmberg <mikkel@brnbw.com>
2015-11-01 10:24:55 +0100

    Go back to using eval

--- dotjs.js
+++ dotjs.js
@@ -1,10 +1,21 @@
-(function(w, d) {
-  'use strict';
+/* global XMLHttpRequest, location */
+var xhr = new XMLHttpRequest()
 
-  d.addEventListener('DOMContentLoaded', function() {
-    var file = 'https://localhost:3131/' + w.location.host.replace('www.', '') + '.js';
-    var scriptElm = document.createElement('script');
-    scriptElm.src = file;
-    d.body.appendChild(scriptElm);
-  });
-})(window, document);
+function apply (script) {
+  if (document.body) {
+    /* eslint-disable no-eval */
+    if (script) eval(script)
+    /* eslint-enable no-eval */
+  } else {
+    setTimeout(function () { apply(script) }, 200)
+  }
+}
+
+var filename = location.hostname.replace(/^www\./, '') + '.js'
+xhr.open('get', 'https://localhost:3131/' + filename, true)
+xhr.responseType = 'text'
+xhr.onload = function (event) {
+  apply(this.responseText)
+}
+xhr.send()
+
